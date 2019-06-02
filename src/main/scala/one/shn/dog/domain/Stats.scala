17cd9afd2f6a4f46ed1ccf0b8f47bb868008aeac
shn-amn <zero@shn.one>
2019-06-02 22:30:41 +0200

    Proper messages.

--- src/main/scala/one/shn/dog/domain/Stats.scala
+++ src/main/scala/one/shn/dog/domain/Stats.scala
@@ -2,14 +2,23 @@ package one.shn.dog.domain
 
 import java.time.Instant
 
+import scala.language.postfixOps
+
 case class Stats(timestamp: Instant, logs: Vector[Log]) {
 
-  private val hits = logs groupBy (_.section) mapValues (_.length)
-  private val maxHits = hits.values.max
+  private lazy val hits = logs groupBy (_.section) mapValues (_.length)
+  private lazy val maxHits = hits.values.max
+  private lazy val popSections = hits collect { case (section, n) if n == maxHits => section }
 
   val count: Int = logs.length
 
   def message: String =
-    hits collect { case (section, n) if n == maxHits => section } mkString ", "
+    if (logs.isEmpty) s"$timestamp: No hits."
+    else popSections match {
+      case head :: Nil =>
+        s"$timestamp: $count hits. Most popular section $head with ${100 * maxHits / count}%."
+      case head :: tail =>
+        s"$timestamp: $count hits. Most popular sections ${tail mkString ", "} and $head with ${100 * maxHits / count}% each."
+    }
 
 }
