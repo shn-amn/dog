af9c143b4e047a400b4d040013be3c061c5c6da7
shn-amn <zero@shn.one>
2019-06-02 22:29:18 +0200

    Better date/time format and regex for logs.

--- src/main/scala/one/shn/dog/domain/Log.scala
+++ src/main/scala/one/shn/dog/domain/Log.scala
@@ -3,21 +3,34 @@ package one.shn.dog.domain
 import java.time.Instant
 import java.time.format.DateTimeFormatter
 
+import scala.language.postfixOps
+import scala.util.Try
 import scala.util.matching.Regex
 
 case class Log(timestamp: Instant, section: String)
 
 object Log {
 
-  // TODO: Is regex too permissive?
-  val log: Regex = raw"""^[0-9.]+ - .* \[([^\]]+)\] "[A-Z]{3,10} (/[a-z]*)[/a-z]* [A-Z/0-9.]+" [0-9]{3} [0-9]+""".r
+  val logLine: Regex = raw"""^[\d.:]+\s\S*-\S*\s\S*\s\[([^\]]+)\]\s"\S*\s(/\S*)\S*\s\S*"\s\d{3}\s\d+""".r
 
-  // TODO: Time zone offset does not seem to work!
-  val format: DateTimeFormatter = DateTimeFormatter ofPattern "dd/MMMM/yyyy:HH:mm:ss xxxx"
+  /**
+    * All accepted date and time formats.
+    */
+  val timeFormats: List[String] = "dd/MMM/yyyy:HH:mm:ss xx" :: "dd/MMMM/yyyy:HH:mm:ss xx" :: Nil
 
+  private val formatters: List[DateTimeFormatter] = timeFormats map DateTimeFormatter.ofPattern
+
+  // Tries to parse date and time with all the formats above, picks the first one working.
+  private def parseTime(s: String): Option[Instant] =
+    (formatters map (fmt => Try(fmt parse (s, Instant from _)) toOption) foldLeft (None: Option[Instant])) (_ orElse _)
+
+  /**
+    * If input string matches `logLine` regex and date/time format is supported, returns parsed log.
+    * Otherwise returns `None`.
+    */
   def parse(line: String): Option[Log] = line match {
-    case log(timestamp, section) => Some(Log(format parse (timestamp, Instant from _), section))
-    case _                       => None
+    case logLine(timestamp, section) => parseTime(timestamp) map (Log(_, section))
+    case _                           => None
   }
 
 }
