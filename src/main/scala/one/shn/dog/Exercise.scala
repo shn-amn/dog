76673be2c98ca633faf992bc8903fc9439337d60
shn-amn <zero@shn.one>
2019-06-02 22:21:27 +0200

    Breaking down streams for better testability.

--- src/main/scala/one/shn/dog/Exercise.scala
+++ src/main/scala/one/shn/dog/Exercise.scala
@@ -11,6 +11,7 @@ import one.shn.dog.Streams._
 import one.shn.dog.out.Display
 
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService}
+import scala.concurrent.duration._
 import scala.language.postfixOps
 
 object Exercise extends IOApp {
@@ -21,6 +22,7 @@ object Exercise extends IOApp {
   def job(since: Instant, threshold: Int): Stream[IO, Unit] =
     Stream resource blockingExecutionContext flatMap { blockingEC =>
       logsSince(Paths get "/tmp/access.log", since, blockingEC)
+        .through(addHeartbeat(lag = 500 millis))
         .through(groupToStats)
         .evalTap(stats => IO(Display transient stats.message))
         .through(scanForAlerts(10))
--- src/main/scala/one/shn/dog/Streams.scala
+++ src/main/scala/one/shn/dog/Streams.scala
@@ -5,7 +5,7 @@ import java.time.Instant
 
 import cats.effect.{Concurrent, ContextShift, IO, Timer}
 import cats.implicits._
-import fs2.{Pipe, Stream}
+import fs2.{Pipe, Pure, Stream}
 import one.shn.dog.domain.{Alert, Busy, Log, Normal, Stats}
 
 import scala.concurrent.ExecutionContext
@@ -34,36 +34,41 @@ object Streams {
   : Stream[IO, Instant] =
     Stream
       .awakeEvery[IO](1 second)
-      .evalMap(_ => timer.clock.realTime(MILLISECONDS) map (_ - lag.toMillis))
+      .evalMap(_ => timer.clock realTime MILLISECONDS map (_ - lag.toMillis))
       .map(Instant.ofEpochMilli)
 
-  def groupToStats(
+  def addHeartbeat(
+      lag:   Duration)(
       implicit
       timer: Timer[IO],
       con:   Concurrent[IO])
-  : Pipe[IO, Log, Stats] = _
+  : Pipe[IO, Log, Either[Instant, Log]] = _
     .map(Right.apply)
-    .merge(heartbeat(lag = 500 millis) map Left.apply)
-    .filterWithPrevious(notDisorderedHeartbeat)
+    .merge(heartbeat(lag) map Left.apply)
+    .filterWithPrevious(notLaggingHeartbeat)
+
+  private def notLaggingHeartbeat(
+      prev: Either[Instant, Log],
+      curr: Either[Instant, Log]) =
+    curr.isRight || !(timestamp(prev) isAfter timestamp(curr))
+
+  def groupToStats
+  : Pipe[Pure, Either[Instant, Log], Stats] = _
     .groupAdjacentBy(timestamp(_).toEpochMilli / 10000)
-    .drop(1) // The first one does not necessarily represent 10 seconds
+    .drop(1) // The first group does not necessarily represent 10 seconds
     .map { case (tenthSeconds, logsAndHeartbeats) => Stats(
       timestamp = Instant ofEpochSecond 10 * (tenthSeconds + 1),
       logs      = logsAndHeartbeats collect { case Right(log) => log } toVector)
     }
-
-  private def notDisorderedHeartbeat(
-      prev: Either[Instant, Log],
-      curr: Either[Instant, Log]) =
-    curr.isRight || !(timestamp(prev) isAfter timestamp(curr))
 
   private def timestamp(x: Either[Instant, Log]) = x match {
     case Left(instant) => instant
     case Right(log)    => log.timestamp
   }
 
-  def scanForAlerts(threshold: Int)
-  : Pipe[IO, Stats, Alert] = _
+  def scanForAlerts(
+      threshold: Int)
+  : Pipe[Pure, Stats, Alert] = _
     .sliding(12) // 2 min = 12 * 10 sec
     .map(stats => (stats map (_.count) sum, stats.last.timestamp))
     .map { case (count, timestamp) => (count > threshold * 120, count, timestamp) } // 2 min = 120 sec
