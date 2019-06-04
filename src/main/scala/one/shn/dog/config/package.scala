db966dbdc258d74dc35408afb142490743333c18
shn-amn <zero@shn.one>
2019-06-04 14:15:01 +0200

    Configuration extracted from programme body to a special package.

--- src/main/scala/one/shn/dog/config/package.scala
+++ src/main/scala/one/shn/dog/config/package.scala
@@ -1,10 +1,18 @@
 package one.shn.dog
 
+import java.nio.file.{Path, Paths}
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
+import scala.language.postfixOps
+
 package object config {
 
-  val fmt: DateTimeFormatter = DateTimeFormatter ofPattern "HH:mm:ss" withZone ZoneId.systemDefault
+  implicit val fmt: DateTimeFormatter = DateTimeFormatter ofPattern "HH:mm:ss" withZone ZoneId.systemDefault
+
+  val logfile:   Path = Paths get env("ACCESS_LOG_PATH", "/tmp/access.log")
+  val threshold: Int  = env("ACTIVITY_ALERT_THRESHOLD", "10") toInt
+
+  private def env(variable: String, default: String) = sys.env getOrElse (variable, default)
 
 }
